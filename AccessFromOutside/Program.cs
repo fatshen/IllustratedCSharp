ac4d27095ee7cba5ecc04204158bcf9a667b8925
Shen Wenjie <sandwich@live.cn>
2019-12-02 23:28:36 +0800

    Add Access From Outside

---
+++ AccessFromOutside/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class DayTemp
+{
+    public int High = 75;
+    public int Low = 45;
+}
+
+namespace AccessFromOutside
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DayTemp temp = new DayTemp();
+
+            temp.High = 80;
+            temp.Low = 40;
+
+            Console.WriteLine("High:    {0}", temp.High);
+            Console.WriteLine("Low:     {0}", temp.Low);
+        }
+    }
+}
