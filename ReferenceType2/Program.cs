be911191c089936ed7466483be724e3218443d52
Shen Wenjie <sandwich@live.cn>
2019-12-09 22:12:49 +0800

    add ReferenceType2

---
+++ ReferenceType2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ReferenceType2
+{
+    class MyClass
+    {
+        public int val = 20;
+    }
+
+    class Program
+    {
+        static void MyMethod(ref MyClass f1)
+        {
+            f1.val = 50;
+            Console.WriteLine("f1.val: {0}", f1.val);
+
+            f1 = new MyClass();
+            Console.WriteLine("f1.val: {0}", f1.val);
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            Console.WriteLine("mc.val: {0}", mc.val);
+
+            MyMethod(ref mc);
+
+            Console.WriteLine("mc.val: {0}", mc.val);
+        }
+    }
+}
