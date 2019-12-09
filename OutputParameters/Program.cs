411221565a01f8f32942e04eaac2e98697e55c1b
Shen Wenjie <sandwich@live.cn>
2019-12-09 22:21:00 +0800

    add OutputParameters

---
+++ OutputParameters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OutputParameters
+{
+    class MyClass
+    {
+        public int val = 20;
+    }
+
+    class Program
+    {
+        static void MyMethod(out MyClass f1, out int f2)
+        {
+            f1 = new MyClass();
+            f1.val = 50;
+            f2 = 15;
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass a1 = null;
+            int a2;
+
+            MyMethod(out a1, out a2);
+            Console.WriteLine("a1.val: {0}, a2: {1}", a1.val, a2);
+        }
+    }
+}
