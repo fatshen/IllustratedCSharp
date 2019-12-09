c698d26b44606da40f3caf080616a151bec6e57f
Shen Wenjie <sandwich@live.cn>
2019-12-09 20:47:37 +0800

    add Method Invocations

---
+++ MethodInvocations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MethodInvocations
+{
+    class MyClass
+    {
+        void PrintDateAndTime()
+        {
+            DateTime dt = DateTime.Now;
+            Console.WriteLine("{0}", dt);
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            myClass.PrintDateAndTime();
+        }
+    }
+}
