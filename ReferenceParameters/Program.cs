4844f3e5a564eb1f5bd155e5cf25fdcfab91b069
Shen Wenjie <sandwich@live.cn>
2019-12-09 21:49:31 +0800

    add ReferenceParameters

---
+++ ReferenceParameters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ValueParameters
+{
+    class MyClass
+    {
+        public int val = 20;
+    }
+
+    class Program
+    {
+        static void MyMethod(MyClass f1, ref int f2)
+        {
+            f1.val = f1.val + 5;
+            f2 = f2 + 5;
+            Console.WriteLine("f1.val: {0}, f2: {1}", f1.val, f2);
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass a1 = new MyClass();
+            int a2 = 10;
+            Console.WriteLine("f1.val: {0}, f2: {1}", a1.val, a2);
+            MyMethod(a1, ref a2);
+            Console.WriteLine("f1.val: {0}, f2: {1}", a1.val, a2);
+        }
+    }
+}
