084df0b5f293b992fb08ea0e37e588ac1bc70a99
Shen Wenjie <sandwich@live.cn>
2019-12-09 20:52:51 +0800

    add Return Values

---
+++ ReturnValues/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReturnValues
+{
+    class MyClass
+    {
+        public int GetHour()
+        {
+            DateTime dt = DateTime.Now;
+            return dt.Hour;
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            Console.WriteLine("Hour: {0}", mc.GetHour());
+        }
+    }
+}
