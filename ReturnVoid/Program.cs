8e6eddb3eb5ed2b5a5d13977ac4ab0a159526d63
Shen Wenjie <sandwich@live.cn>
2019-12-09 20:58:30 +0800

    add ReturnVoid

---
+++ ReturnVoid/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReturnVoid
+{
+    class MyClass
+    {
+        void TimeUpdate()
+        {
+            DateTime dt = DateTime.Now;
+            if (dt.Hour < 12)
+            {
+                return;
+            }
+
+            Console.WriteLine("It's afternoon!");
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            mc.TimeUpdate();
+        }
+    }
+}
