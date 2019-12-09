351896bcb5dae657c4492ce8d0232d2a4b56a847
Shen Wenjie <sandwich@live.cn>
2019-12-09 21:04:31 +0800

    add PositionalParameters

---
+++ PositionalParameters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PositionalParameters
+{
+    class MyClass
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public float Avg(float x, float y)
+        {
+            return (x + y) / 2;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            int SomeInt = 6;
+
+            Console.WriteLine("Sum: {0} and {1} is {2}", 5, SomeInt, mc.Sum(5, SomeInt));
+            Console.WriteLine("Avg: {0} and {1} is {2}", 5, SomeInt, mc.Avg(5, SomeInt));
+        }
+    }
+}
